52b4bb01284f30bcacb65ba943fdc6c7ae306ec7
lambda_decv <bzdalexis@gmail.com>
2022-11-18 11:11:17 +0100

    change mediapipe hands version

--- hands.js
+++ hands.js
@@ -1,73 +1,29 @@
 const video3 = document.getElementsByClassName('input_video3')[0];
 const out3 = document.getElementsByClassName('output3')[0];
 const controlsElement3 = document.getElementsByClassName('control3')[0];
-const canvasCtx3 = out3.getContext('2d');
+const canvasCtx = out3.getContext('2d');
 const fpsControl = new FPS();
 let width = 1280;
 let height = 720;
 
-// async function getWebcamResolution() {
-//   let resolution = new Array();
-  
-//   let features = {
-//       audio: false,
-//       video: true
-//   };
-
-//   let display = await navigator.mediaDevices
-//       .getUserMedia(features);
-
-//   let settings = display.getVideoTracks()[0]
-//       .getSettings();
-
-//   return new Object({
-//       width: settings.width,
-//       height: settings.height
-//   });
-// }
-
-// width = getWebcamResolution().then(function(result){
-//   return result.width;
-// });
-// height = getWebcamResolution().then(function(result){
-//   return result.height;
-// });
-
-// alert("Width : " + width + " Height : " + height);
-
-console.log(canvasCtx3.width, canvasCtx3.height); // 0 0                                        
-
-function onResultsHands(results) {
-  document.body.classList.add('loaded');
-  fpsControl.tick();
-
-  canvasCtx3.save();
-  canvasCtx3.clearRect(0, 0, out3.width, out3.height);
-  canvasCtx3.drawImage(results.image, 0, 0, out3.width, out3.height);
-  if (results.multiHandLandmarks && results.multiHandedness) {
-    for (let index = 0; index < results.multiHandLandmarks.length; index++) {
-      const classification = results.multiHandedness[index];
-      const isRightHand = classification.label === 'Right';
-      const landmarks = results.multiHandLandmarks[index];
-      drawConnectors(
-          canvasCtx3, landmarks, HAND_CONNECTIONS,
-          {color: isRightHand ? '#00FF00' : '#FF0000'}),
-      drawLandmarks(canvasCtx3, landmarks, {
-        color: isRightHand ? '#0000FF' : '#FF0000',
-        fillColor: isRightHand ? '#0000FF' : '#00FF00',
-        radius: (x) => {
-          return lerp(x.from.z, -0.15, .1, 10, 1)/10;
-        }
-      });
+function onResults(results) {
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, out3.width, out3.height);
+  canvasCtx.drawImage(
+      results.image, 0, 0, out3.width, out3.height);
+  if (results.multiHandLandmarks) {
+    for (const landmarks of results.multiHandLandmarks) {
+      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
+                     {color: '#00FF00', lineWidth: 5});
+      drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
     }
   }
-  canvasCtx3.restore();
+  canvasCtx.restore();
 }
 
 const hands = new Hands({locateFile: (file) => {
-  return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.3/${file}`;
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
 }});
-hands.onResults(onResultsHands);
 
 const camera = new Camera(video3, {
   onFrame: async () => {
@@ -76,38 +32,17 @@ const camera = new Camera(video3, {
   width: 1280,
   height: 720
 });
-
+hands.setOptions({
+  selfieMode: true,
+  maxNumHands: 2,
+  modelComplexity: 0,
+  minDetectionConfidence: 0.90,
+  minTrackingConfidence: 0.90
+  
+});
+hands.onResults(onResults);
 camera.start();
 
-new ControlPanel(controlsElement3, {
-      selfieMode: true,
-      maxNumHands: 1,
-      minDetectionConfidence: 0.5,
-      minTrackingConfidence: 0.5
-        })
-    .add([
-      new StaticText({title: 'MediaPipe Hands'}),
-      fpsControl,
-      new Toggle({title: 'Selfie Mode', field: 'selfieMode'}),
-      new Slider(
-          {title: 'Max Number of Hands', field: 'maxNumHands', range: [0,2], step: 1}),
-      new Slider({
-        title: 'Min Detection Confidence',
-        field: 'minDetectionConfidence',
-        range: [0, 1],
-        step: 0.01
-      }),
-      new Slider({
-        title: 'Min Tracking Confidence',
-        field: 'minTrackingConfidence',
-        range: [0, 1],
-        step: 0.01
-      }),
-    ])
-    .on(options => {
-      video3.classList.toggle('selfie', options.selfieMode);
-      hands.setOptions(options);
-    });
 
 let touchstartX = 0
 let touchendX = 0
