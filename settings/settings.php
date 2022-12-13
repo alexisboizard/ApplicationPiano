97b6552603e8d7c3323f8eea772c88d1d8986d58
johan <johan.gagean@gmail.com>
2022-12-13 17:18:57 +0100

    update settings

--- settings/settings.php
+++ settings/settings.php
@@ -14,7 +14,7 @@
 <body>
     <header>
         <a href="home.php"><img class="back" src="img-sae/back.svg" alt=""></a>
-        <h1>Paramètres</h1>
+        <h1>Paramètres (Alpha)</h1>
     </header>
     
     <main>
