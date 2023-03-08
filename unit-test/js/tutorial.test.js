ad907b96f38c797a0e6a4f9fefc63ae957bbbfe4
johan <johan.gagean@gmail.com>
2023-03-08 17:42:46 +0100

    Add first unit test in JS (not complete)

---
+++ unit-test/js/tutorial.test.js
@@ -0,0 +1,23 @@
+/* Tests tutorial.js */
+
+function assert(condition, message)
+{
+    if (!condition) {
+        throw new Error(message);
+      }
+}
+
+function testNext()
+{
+    assert(/* condition, testNext doit retourner... */);
+}
+
+function testPrev()
+{
+    assert(/* condtion, testPrev doit retourner... */);
+}
+
+function testClosetuto()
+{
+    assert(/* condtion, testCloseTuto doit retourner... */);
+}
